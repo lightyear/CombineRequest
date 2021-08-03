35f07265f06388eca9b3848d09ed9791104f3c67
Steve Madsen <steve@lightyearsoftware.com>
2021-08-03 18:08:04 -0400

    Move operators to their own file

--- Sources/CombineRequest/APIBase.swift
+++ Sources/CombineRequest/APIBase.swift
@@ -116,35 +116,3 @@ open class APIBase: ObservableObject {
         }
     }
 }
-
-extension Publisher {
-    public func isHTTPResponse() -> Publishers.TryMap<Self, APIBase.DataResponseTuple> where Output == (data: Data, response: URLResponse) {
-        self.tryMap {
-            if let httpResponse = $0.response as? HTTPURLResponse {
-                return (data: $0.data, response: httpResponse)
-            }
-            throw RequestError.nonHTTPResponse
-        }
-    }
-
-    public func validateStatusCode<Codes: Sequence>(in statusCodes: Codes) -> Publishers.TryMap<Self, Output> where Codes.Element == Int, Output == APIBase.DataResponseTuple {
-        self.tryMap {
-            if !statusCodes.contains($0.response.statusCode) {
-                throw RequestError.httpFailure($0.response.statusCode)
-            }
-            return $0
-        }
-    }
-
-    public func hasContentType(_ expectedType: String) -> Publishers.TryMap<Self, Output> where Output == APIBase.DataResponseTuple {
-        self.tryMap {
-            guard !$0.data.isEmpty else { return $0 }
-            if let contentType = $0.response.value(forHTTPHeaderField: "Content-Type") {
-                if contentType == expectedType || contentType.hasPrefix("\(expectedType); charset=") {
-                    return $0
-                }
-            }
-            throw RequestError.contentTypeMismatch
-        }
-    }
-}
